74469fd70a5357022d5efb4c79bd0b5e34d77200
tk w <tkgamegroup@outlook.com>
2023-02-02 11:08:21 +0800

    rename

--- assets/shaders/post_shading.glsl
+++ assets/shaders/post_shading.glsl
@@ -1,14 +1,14 @@
 #ifdef VISION_MAP_ID
 	float war_fog = 0.0;
-	war_fog += sample_map(VISION_MAP_ID, coordw.xz / vec2(256)).r * 2;
-	war_fog += sample_map(VISION_MAP_ID, (coordw.xz + vec2(-1, 0)) / vec2(256)).r;
-	war_fog += sample_map(VISION_MAP_ID, (coordw.xz + vec2(+1, 0)) / vec2(256)).r;
-	war_fog += sample_map(VISION_MAP_ID, (coordw.xz + vec2(0, +1)) / vec2(256)).r;
-	war_fog += sample_map(VISION_MAP_ID, (coordw.xz + vec2(0, -1)) / vec2(256)).r;
-	war_fog += sample_map(VISION_MAP_ID, (coordw.xz + vec2(-1, -1)) / vec2(256)).r * 0.7;
-	war_fog += sample_map(VISION_MAP_ID, (coordw.xz + vec2(+1, +1)) / vec2(256)).r * 0.7;
-	war_fog += sample_map(VISION_MAP_ID, (coordw.xz + vec2(-1, +1)) / vec2(256)).r * 0.7;
-	war_fog += sample_map(VISION_MAP_ID, (coordw.xz + vec2(+1, -1)) / vec2(256)).r * 0.7;
+	war_fog += sample_map(VISION_MAP_ID, world_pos.xz / vec2(256)).r * 2;
+	war_fog += sample_map(VISION_MAP_ID, (world_pos.xz + vec2(-1, 0)) / vec2(256)).r;
+	war_fog += sample_map(VISION_MAP_ID, (world_pos.xz + vec2(+1, 0)) / vec2(256)).r;
+	war_fog += sample_map(VISION_MAP_ID, (world_pos.xz + vec2(0, +1)) / vec2(256)).r;
+	war_fog += sample_map(VISION_MAP_ID, (world_pos.xz + vec2(0, -1)) / vec2(256)).r;
+	war_fog += sample_map(VISION_MAP_ID, (world_pos.xz + vec2(-1, -1)) / vec2(256)).r * 0.7;
+	war_fog += sample_map(VISION_MAP_ID, (world_pos.xz + vec2(+1, +1)) / vec2(256)).r * 0.7;
+	war_fog += sample_map(VISION_MAP_ID, (world_pos.xz + vec2(-1, +1)) / vec2(256)).r * 0.7;
+	war_fog += sample_map(VISION_MAP_ID, (world_pos.xz + vec2(+1, -1)) / vec2(256)).r * 0.7;
 	war_fog /= 8.8;
 	ret *= mix(0.2, 1.0, war_fog);
 #endif
