6bd68c04144c740d3280e1dd83538c3a5b4b1275
tk w <tkgamegroup@outlook.com>
2023-03-27 18:10:42 +0800

    1

--- app.cpp
+++ app.cpp
@@ -7,7 +7,7 @@ UniverseApplication app;
 int main()
 {
 	Path::set_root(L"assets", std::filesystem::current_path() / L"assets");
-	app.create("dota_pubg", uvec2(1280, 720), WindowFrame | WindowResizable, true, { { "mesh_shader"_h, 0 } });
+	app.create("dota_pubg", uvec2(1280, 720), WindowFrame | WindowResizable, false, true, { { "mesh_shader"_h, 0 } });
 	app.world->root->load(L"assets/launcher.prefab");
 	app.renderer->bind_window_targets();
 	app.renderer->mode = sRenderer::CameraLight;
